7e1ef2b91ea58eee58dfdeca7b3d58dea9b852d0
kaufon <kauanfvale@gmail.com>
2025-05-16 09:52:38 -0300

    refactor recipe_finder

--- src/recipe_finder/agent.py
+++ src/recipe_finder/agent.py
@@ -4,7 +4,7 @@ from google.adk.agents import Agent
 from google.adk.tools import google_search
 
 root_agent = Agent(
-    model="gemini-2.0-flash-001",
+    model="gemini-2.0-flash",
     name="recipe_finder_agent",
     description=dedent("""
             An expert culinary researcher with ability to find best recipes online.
@@ -26,7 +26,7 @@ Busque receitas que sigam os seguintes critérios (fornecidos pelo usuário):
 1. Entregue os links das receitas em **formato markdown clicável**, como por exemplo: [Nome da Receita](https://exemplo.com)
 2. Cada link deve conter:
    - ✅ Nome da receita
-   - ✅ Link (formato markdown)
+   - ✅ Link (formato clickavel)
    - ✅ Descrição curta
    - ✅ Tags explicando como ela atende aos critérios do usuário
 3. Não forneça instruções de preparo, apenas os dados acima.
